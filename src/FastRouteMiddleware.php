f104c3091625f878c2243eef22a654a21b801430
Andrey Reinwald <andreinwald@gmail.com>
2017-05-05 01:49:40 +0100

    Created first version

---
+++ src/FastRouteMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class FastRouteMiddleware implements MiddlewareInterface
+{
+    private $routes;
+    private $notFoundCallable;
+
+    public function construct(array $routes, $notFoundCallable)
+    {
+        $this->routes = $routes;
+        $this->notFoundCallable = $notFoundCallable;
+    }
+
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+    {
+        $method = strtoupper($request->getMethod());
+        $uri = strtolower($request->getUri()->getFragment());
+
+        // Process routes
+        $dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+            foreach ($this->routes as $route) {
+                $r->addRoute($route[0], $route[1], $route[2]);
+            }
+        });
+
+        // Dispatch request
+        $routeInfo = $dispatcher->dispatch($method, $uri);
+
+        // Check found
+        if ($routeInfo[0] !== \FastRoute\Dispatcher::FOUND) {
+            return call_user_func_array($this->notFoundCallable, []);
+        }
+
+        // Handle and return created Response
+        // no delegate next here
+        return call_user_func_array($routeInfo[1], $routeInfo[2]);
+    }
+}
